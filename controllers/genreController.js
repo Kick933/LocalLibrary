9776fcc7719c516a83e79af5894b8f61630fb05a
Pavitar Kumar <pavitrsheoran003@gmail.com>
2022-02-17 10:47:03 +0530

    Genre Deletion implemented.

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -81,11 +81,48 @@ exports.genre_create_post = [
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = function(req,res) {
-    res.send("NOT IMPLEMENTED : Genre delete GET")
+    async.parallel({
+        genre:function(callback){
+            Genre.findById(req.params.id).exec(callback)
+        },
+        genre_books:function(callback){
+            Book.find({'genre' : req.params.id},'title summary').exec(callback)
+        }
+    },function(err,result) {
+        if(err) return next(err)
+        if(result.genre === null){
+            res.redirect('/catalog/genres')
+            return
+        }
+        res.render('genre_delete', { genre : result.genre, genre_books : result.genre_books})
+    })
 }
 // Handle Genre delete on POST.
 exports.genre_delete_post = function(req,res) {
-    res.send("NOT IMPLEMENTED : Genre delete POST")
+    async.parallel({
+        genre:function(callback){
+            Genre.findById(req.body.genreid).exec(callback)
+        },
+        genre_books:function(callback){
+            Book.find({'genre' : req.body.genreid},'title summary').exec(callback)
+        }
+    },function(err,result) {
+        if(err) return next(err)
+        // If genre not found, redirect to genre list
+        if(result.genre === null){
+            res.redirect('/catalog/genres')
+            return
+        }
+        // render genre_delete page if books are still linked.
+        if(result.genre_books.length > 0){
+            res.render('genre_delete', { genre : result.genre, genre_books : result.genre_books})
+            return
+        }
+        Genre.findByIdAndRemove(req.body.genreid, function(err){
+            if(err) return next(err)
+            res.redirect('/catalog/genres')
+        })
+    })
 }
 // Display Genre update form on GET.
 exports.genre_update_get = function(req,res) {
